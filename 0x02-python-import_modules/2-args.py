59306f4341620dca315d5efe4cfedc40297203c5
kwakye02 <kwakyegeorge0@gmail.com>
2023-10-11 09:57:33 -0700

    task 3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,11 @@ from sys import argv
 if __name__ == "__main__":
     if (len(argv)-1) == 1:
         print("{} argument:".format(len(argv) - 1))
-        print("{}: {}".format(1,argv[1]))
+        print("{}: {}".format(1, argv[1]))
 
     elif (len(argv)-1) > 1:
         print("{} arguments:".format(len(argv) - 1))
-        for i in range(1,len(argv)):
-            print("{}: {}".format(i,argv[i]))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
     else:
         print("{} arguments.".format(len(argv) - 1))
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+if __name__ == "__main":
+    sum = 0
+
+    if (len(argv) - 1) < 1:
+        print("{}".format(sum))
+    else:
+        for i in range(1, len(argv)):
+            sum += int(argv[i])
+        print("{}".format(sum))
